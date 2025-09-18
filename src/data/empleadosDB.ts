0fee6768ada4ad5e0980d47e01aaf619bb6c04f6
JUAN RODRIGUEZ TABARES <Juan.RTabares@alumno.udg.mx>
2025-09-18 17:28:06 -0600

    Añadir Supervisores

--- src/data/empleadosDB.ts
+++ src/data/empleadosDB.ts
@@ -1,160 +1,160 @@
-import type { Empleado } from "../types"
+import type { Empleado } from "../types";
 
-export const db : Empleado[] = [
+export const db: Empleado[] = [
   {
-    name: 'Johana Araceli Lopez Lomeli',
-    folio: '427051',
-    puesto: 'Coordinadora Municipal',
-    foto: '/imagenes/JohanaLL.jpg'
+    name: "Johana Araceli Lopez Lomeli",
+    folio: "427051",
+    puesto: "Coordinadora Municipal",
+    foto: "/imagenes/JohanaLL.jpg",
   },
   {
-    name: 'Reyna Itzel Peña Ruiz',
-    folio: '803648',
-    puesto: 'Auxiliar Administrativo de la Coordinacion Municipal',
-    foto: '/imagenes/ReynaPZ.jpg'
+    name: "Reyna Itzel Peña Ruiz",
+    folio: "803648",
+    puesto: "Auxiliar Administrativo de la Coordinacion Municipal",
+    foto: "/imagenes/ReynaPZ.jpg",
   },
   {
-    name: 'Juan Rodriguez Tabares',
-    folio: '803446',
-    puesto: 'Soporte Informatico Municipal',
-    foto: '/imagenes/JuanRT.jpg'
+    name: "Juan Rodriguez Tabares",
+    folio: "803446",
+    puesto: "Soporte Informatico Municipal",
+    foto: "/imagenes/JuanRT.jpg",
   },
   {
-    name: 'Daniel Bervera Gutierrez',
-    folio: '458632',
-    puesto: 'Responsable de Area',
-    foto: '/imagenes/DanielBG.jfif'
+    name: "Daniel Bervera Gutierrez",
+    folio: "458632",
+    puesto: "Responsable de Area",
+    foto: "/imagenes/DanielBG.jfif",
   },
   {
-    name: 'Anna Maria Mendoza Rodriguez',
-    folio: '799006',
-    puesto: 'Responsable de Area',
-    foto: '/imagenes/AnnaMMR.jfif'
+    name: "Anna Maria Mendoza Rodriguez",
+    folio: "799006",
+    puesto: "Responsable de Area",
+    foto: "/imagenes/AnnaMMR.jfif",
   },
   {
-    name: 'Nancy Elizabeth Garcia Magaña',
-    folio: '507742',
-    puesto: 'Responsable de Area',
-    foto: '/imagenes/NancyEGM.jfif'
+    name: "Nancy Elizabeth Garcia Magaña",
+    folio: "507742",
+    puesto: "Responsable de Area",
+    foto: "/imagenes/NancyEGM.jfif",
   },
   {
-    name: 'Lissette Marisol Padron Alvarado',
-    folio: '567402',
-    puesto: 'Responsable de Area',
-    foto: '/imagenes/LissetteMPA.jfif'
+    name: "Lissette Marisol Padron Alvarado",
+    folio: "567402",
+    puesto: "Responsable de Area",
+    foto: "/imagenes/LissetteMPA.jfif",
   },
   {
-    name: 'Ana Paola Cervantes Contreras',
-    folio: '578206',
-    puesto: 'Supervisor/a de Entrevistadores',
-    foto: '/imagenes/perfil.jpg'
+    name: "Ana Paola Cervantes Contreras",
+    folio: "578206",
+    puesto: "Supervisor/a de Entrevistadores",
+    foto: "/imagenes/supervisores/AnaPCC.jpg",
   },
   {
-    name: 'Isaura Contreras Magana',
-    folio: '808670',
-    puesto: 'Supervisor/a de Entrevistadores',
-    foto: '/imagenes/perfil.jpg'
+    name: "Isaura Contreras Magana",
+    folio: "808670",
+    puesto: "Supervisor/a de Entrevistadores",
+    foto: "/imagenes/supervisores/IsauraCM.jpg",
   },
   {
-    name: 'Ana Faviola Corona Hernandez',
-    folio: '545480',
-    puesto: 'Supervisor/a de Entrevistadores',
-    foto: '/imagenes/perfil.jpg'
+    name: "Ana Faviola Corona Hernandez",
+    folio: "545480",
+    puesto: "Supervisor/a de Entrevistadores",
+    foto: "/imagenes/supervisores/AnaFCH.jpg",
   },
   {
-    name: 'Marlen Lopez Valdivia',
-    folio: '808637',
-    puesto: 'Supervisor/a de Entrevistadores',
-    foto: '/imagenes/perfil.jpg'
+    name: "Marlen Lopez Valdivia",
+    folio: "808637",
+    puesto: "Supervisor/a de Entrevistadores",
+    foto: "/imagenes/supervisores/MarlenLV.jpg",
   },
   {
-    name: 'Maria Elena Gallegos Betancourt',
-    folio: '808667',
-    puesto: 'Supervisor/a de Entrevistadores',
-    foto: '/imagenes/perfil.jpg'
+    name: "Maria Elena Gallegos Betancourt",
+    folio: "808667",
+    puesto: "Supervisor/a de Entrevistadores",
+    foto: "/imagenes/supervisores/MariaEGB.jpg",
   },
   {
-    name: 'Ruth Cumplido Rivera',
-    folio: '808706',
-    puesto: 'Supervisor/a de Entrevistadores',
-    foto: '/imagenes/perfil.jpg'
+    name: "Ruth Cumplido Rivera",
+    folio: "808706",
+    puesto: "Supervisor/a de Entrevistadores",
+    foto: "/imagenes/supervisores/RuthCR.jpg",
   },
   {
-    name: 'Elizabeth Natali Lopez Haro',
-    folio: '232975',
-    puesto: 'Supervisor/a de Entrevistadores',
-    foto: '/imagenes/perfil.jpg'
+    name: "Elizabeth Natali Lopez Haro",
+    folio: "232975",
+    puesto: "Supervisor/a de Entrevistadores",
+    foto: "/imagenes/supervisores/ElizabethNLH.jpg",
   },
   {
-    name: 'Anahi Carina Manzo Garcia',
-    folio: '193634',
-    puesto: 'Supervisor/a de Entrevistadores',
-    foto: '/imagenes/perfil.jpg'
+    name: "Anahi Carina Manzo Garcia",
+    folio: "193634",
+    puesto: "Supervisor/a de Entrevistadores",
+    foto: "/imagenes/supervisores/AnahiCMG.jpg",
   },
   {
-    name: 'Ana Luiza Martinez Buenrostro',
-    folio: '472552',
-    puesto: 'Supervisor/a de Entrevistadores',
-    foto: '/imagenes/perfil.jpg'
+    name: "Ana Luiza Martinez Buenrostro",
+    folio: "472552",
+    puesto: "Supervisor/a de Entrevistadores",
+    foto: "/imagenes/supervisores/AnaMartinezB.jpg",
   },
   {
-    name: 'Rosalba Muro Sandoval',
-    folio: '808704',
-    puesto: 'Supervisor/a de Entrevistadores',
-    foto: '/imagenes/perfil.jpg'
+    name: "Rosalba Muro Sandoval",
+    folio: "808704",
+    puesto: "Supervisor/a de Entrevistadores",
+    foto: "/imagenes/supervisores/RosalbaMS.jpg",
   },
   {
-    name: 'Ana Gabriela Magana Cardenas',
-    folio: '253650',
-    puesto: 'Supervisor/a de Entrevistadores',
-    foto: '/imagenes/perfil.jpg'
+    name: "Ana Gabriela Magana Cardenas",
+    folio: "253650",
+    puesto: "Supervisor/a de Entrevistadores",
+    foto: "/imagenes/supervisores/AnaMaganaC.jpg",
   },
   {
-    name: 'Cynthia Angelica Lopez Medrano',
-    folio: '512719',
-    puesto: 'Supervisor/a de Entrevistadores',
-    foto: '/imagenes/perfil.jpg'
+    name: "Cynthia Angelica Lopez Medrano",
+    folio: "512719",
+    puesto: "Supervisor/a de Entrevistadores",
+    foto: "/imagenes/supervisores/CynthiaLopezM.jpg",
   },
   {
-    name: 'Maria Guadalupe Arias Lopez',
-    folio: '269743',
-    puesto: 'Supervisor/a de Entrevistadores',
-    foto: '/imagenes/perfil.jpg'
+    name: "Maria Guadalupe Arias Lopez",
+    folio: "269743",
+    puesto: "Supervisor/a de Entrevistadores",
+    foto: "/imagenes/supervisores/MariaAriasL.jpg",
   },
   {
-    name: 'Gabriela Gallardo Preciado',
-    folio: '466156',
-    puesto: 'Supervisor/a de Entrevistadores',
-    foto: '/imagenes/perfil.jpg'
+    name: "Gabriela Gallardo Preciado",
+    folio: "466156",
+    puesto: "Supervisor/a de Entrevistadores",
+    foto: "/imagenes/supervisores/GabrielaGP.jpg",
   },
   {
-    name: 'Jorge Hernandez Isidro',
-    folio: '808662',
-    puesto: 'Supervisor/a de Entrevistadores',
-    foto: '/imagenes/perfil.jpg'
+    name: "Jorge Hernandez Isidro",
+    folio: "808662",
+    puesto: "Supervisor/a de Entrevistadores",
+    foto: "/imagenes/supervisores/Isidro.jpg",
   },
   {
-    name: 'Ana Lilia Terriquez Soto',
-    folio: '267514',
-    puesto: 'Supervisor/a de Entrevistadores',
-    foto: '/imagenes/perfil.jpg'
+    name: "Ana Lilia Terriquez Soto",
+    folio: "267514",
+    puesto: "Supervisor/a de Entrevistadores",
+    foto: "/imagenes/supervisores/TERRIQUEZ.jpg",
   },
   {
-    name: 'Maritza Vivilu Rodriguez Granados',
-    folio: '463110',
-    puesto: 'Supervisor/a de Entrevistadores',
-    foto: '/imagenes/perfil.jpg'
+    name: "Maritza Vivilu Rodriguez Granados",
+    folio: "463110",
+    puesto: "Supervisor/a de Entrevistadores",
+    foto: "/imagenes/supervisores/MaritzaVRG.jpg",
   },
   {
-    name: 'Yahayra Anahi Rangel Marquez',
-    folio: '808711',
-    puesto: 'Supervisor/a de Entrevistadores',
-    foto: '/imagenes/perfil.jpg'
+    name: "Yahayra Anahi Rangel Marquez",
+    folio: "808711",
+    puesto: "Supervisor/a de Entrevistadores",
+    foto: "/imagenes/supervisores/YahayraARM.jpg",
   },
   {
-    name: 'Alejandra Renteria Flores',
-    folio: '808683',
-    puesto: 'Supervisor/a de Entrevistadores',
-    foto: '/imagenes/perfil.jpg'
-  }
-]
+    name: "Alejandra Renteria Flores",
+    folio: "808683",
+    puesto: "Supervisor/a de Entrevistadores",
+    foto: "/imagenes/supervisores/AlejandraRF.jpg",
+  },
+];
